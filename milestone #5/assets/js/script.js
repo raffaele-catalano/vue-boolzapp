3e5caff215d56c2b3a30762545169a2069a7e1b0
raffaele-catalano <raf.catalano@gmail.com>
2023-04-20 13:34:39 +0200

    added several comments in script.js + minor changes

--- milestone #5/assets/js/script.js
+++ milestone #5/assets/js/script.js
@@ -13,19 +13,29 @@ const {createApp} = Vue;
 createApp ({
     data() {
         return {
+            // array principale importato dal db.js
             contacts,
+            // contatore
             counter         : 0,
+            // proprietà relativa ad un nuovo messaggio (inviato) con stringa vuota 
             newMessage      : '',
+            // proprietà con stringa vuota utilizzata per la funzione di ricerca chat nella barra laterale
             userFilter      : '',
+            // proprietà relativa alla riproduzione del suono invio/ricezione messaggi
             notification    : true,
+            // proprietà relativa al dropdown menu 
             dropdownChevron : false,
+            // indice dei messaggi inizializzata e utilizzata per consentire l'eliminazione di tutti i messaggi
+            // presenti in una conversazione (e nell'array di oggetti *messages* dell'array di oggetti *contacts*)
             messageIndex    : 0,
+            // due proprietà utilizzate per animare le icone del dropdown menu
             isHoverDelete   : false,
             isHoverInfo     : false,
         }
     },
 
     methods: {
+        // funzione che serve a richiamare al @click la conversazione sulla barra laterale 
         chatSwap(index){
             this.counter = index;
                 console.log('ho cliccato la chat con indice -->', this.counter);
@@ -51,11 +61,10 @@ createApp ({
             this.autoReply();
             // si resetta newMessage in modo che il campo di input venga svuotato
             this.newMessage = '';
-
+            // viene richiamata la funzione di scroll della conversazione
             this.autoScrollChat();
         },
         // questa funzione è relativa alla risposta automatica dopo l'invio di un messaggio
-        // 
         autoReply() {
             setTimeout(() => {
                 const message = {
@@ -67,19 +76,22 @@ createApp ({
                 this.contacts[this.counter].messages.push(message);
             // riproduce suono di ricezione messaggio
                 this.notification ? soundMsgReceived.play() : null;
-
+                // viene richiamata la funzione di scroll della conversazione
                 this.autoScrollChat();
                 // la risposta automatica avviene dopo 1.5 secondi
             }, 1500);
         },
-
+        // questa funzione fa in modo che quando il riquadro della conversazione attiva risulta
+        // "pieno" *scrolli* automaticamente verso il basso
+        // è una funzione di setTimeout con un delay di 1 millisecondo dato per evitare un ritardo
+        // dato dal .push dei messaggi inviati e ricevuti
         autoScrollChat () {
             setTimeout(() => {
                 const chatShowed = document.querySelector('.conversation-showed');
                 chatShowed.scrollTop = chatShowed.scrollHeight;
             }, 1);
         },
-
+        //questa funzione si occupa dell'eliminazione di un determinato messaggio
         deleteMessage(i){
             this.contacts[this.counter].messages.splice(i,1)
         },
